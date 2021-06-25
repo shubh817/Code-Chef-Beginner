c4f8e605698d1dd182d6ccf4faac408fcf1e632b
shubh817 <59890154+shubh817@users.noreply.github.com>
2021-06-25 12:11:30 +0530

    Create Chef and Snackdown.java

---
+++ Chef and Snackdown.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		try{
+		    Scanner sc  = new Scanner(System.in);
+        	int t = sc.nextInt();
+        	for(int i=0;i<t;i++)
+        	{
+        	    int n = sc.nextInt();
+        	    if(n==2010 || n==2015 || n==2016 || n==2017 || n==2019){
+        	        System.out.println("HOSTED");
+        	    }
+        	    else{
+        	        System.out.println("NOT HOSTED");
+        	    }
+        	}
+		}
+		catch(Exception e) {
+		    System.out.println(e);
+		}
+	}
+}

82acc418626e3f9ffd90cf8c8b36afe60971ed5a
shubh817 <59890154+shubh817@users.noreply.github.com>
2021-07-02 18:28:16 +0530

    Create Pair me.java

---
+++ Pair me.java
@@ -0,0 +1,36 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try{
+		    Scanner sc = new Scanner(System.in);
+        	int t=sc.nextInt();
+        	for(int i=0;i<t;i++)
+        	{
+                int a=sc.nextInt();
+                int b=sc.nextInt();
+                int c=sc.nextInt();
+                if(a+b==c || b+c==a || c+a==b)
+                {
+                    System.out.println("YES");
+                }
+                else
+                {
+                    System.out.println("NO");
+                }
+        	}
+		}
+		catch(Exception e)
+		{
+		    System.out.println(e);
+		}
+	}
+}
